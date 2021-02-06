2bac4f8100d2429c3f7ad9a567bfaaca3ec440a5
12ecarter <ecarter9@villanova.edu>
2021-02-06 11:26:43 -0500

    add image from an image folder

---
+++ images.py
@@ -0,0 +1,9 @@
+hat = pygame.images.load("images/hat.png")
+water = pygame.images.load("images/water.png")
+dog = pygame.images.load("images/dog.png")
+ball = pygame.images.load("images/ball.png")
+bowtie = pygames.image.load("images/bowtie.png")
+bow = pygame.images.load("images/bow.png")
+tie = pygame.images.load("images/tie.png")
+
+
